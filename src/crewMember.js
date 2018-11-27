756a44b1049c3351bab3746aae093895f86b876a
Joseph Arias <arias.joseph.a@gmail.com>
2018-11-27 18:00:55 -0500

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,32 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position) {
+    this.position = position;
+    this.currentShip = 'Looking for a Rig';
+    if(this.currentShip === 'Looking for a Rig') {
+      this.x = 'had no effect';
+    }
+  }
+
+  engageWarpDrive() {
+    if (this.currentShip === 'Looking for a Rig') {
+      return 'had no effect';
+    }
+  }
+
+  setsInvisibility() {
+    if (this.position === 'Defender' && this.currentShip != 'Looking for a Rig') {
+      this.currentShip.cloaked = true
+    } else {
+      return 'had no effect'
+    }
+  }
+
+  chargePhasers() {
+    if (this.currentShip === 'Looking for a Rig') {
+      return 'had no effect';
+    }
+  }
+
+}
+
+
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,26 @@
-class Spaceship {}
+class Spaceship {
+  
+  constructor(name, crew, phasers, shields) {
+    this.name = name;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.crew = crew;
+    this.warpDrive = 'disengaged';
+    this.phasersCharge = 'uncharged';
+    this.docked = crew.length ? false : true;
+    this.setCrew();
+  }
+
+  setCrew() {
+    this.crew.forEach(member => {
+      member.currentShip = this
+    });
+  }
+
+
+
+
+}
+
+

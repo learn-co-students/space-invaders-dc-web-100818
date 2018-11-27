8394548da1bbb3670f7c594b028619ac54dcd218
imanj12 <imanj12@gmail.com>
2018-11-27 15:38:34 -0500

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,16 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name
+    this.crew = crew
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = 'disengaged'
+    this.phasersCharge = 'uncharged'
+    !!this.crew.length ? this.docked = false : this.docked = true
+    this.attachCrew()
+  }
+  attachCrew() {
+    this.crew.forEach(crewMember => crewMember.currentShip = this)
+  }
+}
